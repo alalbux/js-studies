2f3a24189960fd793e6c2dd4e9882b2a047c4a10
Amanda Albuquerque <amandlei@hotmail.com>
2019-01-19 04:38:41 -0200

    studies: program structure

---
+++ program-structure/index.js
@@ -0,0 +1,40 @@
+// Estrutura de programa
+
+// Expressões e Afirmações (expressions and statements)
+
+console.log(1)
+console.log(!false)
+
+// Variáveis (binding or variable)
+// var = global scope
+// let = scope
+
+let caught = 5 * 5
+console.log(caught)
+
+let ten = 10
+
+console.log(ten * ten)
+
+let mood = "light"
+console.log(mood)
+
+mood = "dark"
+console.log(mood)
+
+let luigisDebt = 140
+luigisDebt = luigisDebt - 35
+
+console.log(luigisDebt)
+
+// Palavras reservadas:
+// break case catch class const continue debugger default
+// delete do else enum export extends false finally for
+// function if implements import interface in instanceof let
+// new package private protected public return static super
+// switch this throw true try typeof var void while with yield
+
+// Funções 
+
+let x = 30
+console.log("the value of x is", x)
